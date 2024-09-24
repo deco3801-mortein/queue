76a139f894c94a4c340fc9097f6d59bb77ed7c48
denzelst <112802399+denzelst@users.noreply.github.com>
2024-09-24 07:39:19 +0000

    Added PlantData model

---
+++ plantMetricHandler/src/plantMetricHandler/plantData.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace plantMetricHandler
+{
+    [Table("plant_data")]
+    public class PlantData
+    {
+        [Key]
+        
+        public int DeviceId { get; set; }
+
+        [Required]
+        [Column("sensor_id")]
+        public string SensorId { get; set; }
+
+        [Required]
+        [Column("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Column("moisture")]
+        public double Moisture { get; set; }
+
+        [Column("sunlight")]
+        public int Sunlight { get; set; }
+
+        [Column("temp")]
+        public int Temp { get; set; }
+
+        [Column("vibration_status")]
+        public bool VibrationStatus { get; set; }
+    }
+}

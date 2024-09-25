fd9701868a76fe94742d70d33c57f6d3d308ec67
denzelst <112802399+denzelst@users.noreply.github.com>
2024-09-25 08:24:13 +0000

    Updated local postgres dev

--- .devcontainer/init.sql
+++ .devcontainer/init.sql
@@ -1,12 +1,8 @@
-
-CREATE DATABASE testdb;
--- Connect to db
-\c testdb;
-
-CREATE TABLE sensor_data (
-    sensor_id VARCHAR(100),
+CREATE TABLE plant_data (
+    sensor_id INTEGER,
     timestamp TIMESTAMP,
     moisture DOUBLE PRECISION,
     sunlight INTEGER,
-    temp INTEGER
+    temp INTEGER,
+    vibration_status BOOLEAN
 );
